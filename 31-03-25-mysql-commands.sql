a90be0f2c7ff55c8e2ceee6a206b3a5211f4169c
Lais-Felipe <laisfaraujo2@gmail.com>
2025-03-31 17:15:56 -0300

    Add files via upload

---
+++ 31-03-25-mysql-commands.sql
@@ -0,0 +1,4 @@
+create database `lais`;
+# a linha acima serve para criar um banco de dados com o nome`lais`
+# o uso da crase se torna necessário para indicar um `string` no sistema
+# `string`do sistema `database,`table` e `campo`
